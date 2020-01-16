d73112428cce7a91824669edc2f50a30cf804215
Valery Koshkin <valkoshkin99@gmail.com>
2020-01-16 15:50:39 +0400

    Починил класс Request

--- src/taskmanager/requests/Request.java
+++ src/taskmanager/requests/Request.java
@@ -1,35 +1,25 @@
 package taskmanager.requests;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import taskmanager.task.Task;
-
-import java.util.LinkedList;
-import java.util.List;
 
 @JsonAutoDetect
 public class Request {
-    //@JsonProperty("task")
-    private String request;
-    @JsonDeserialize(as = LinkedList.class)
-    private List<Task> journal;
+    private String command;
+    private Object data;
 
-    public Request() {
+    public String getCommand(){
+        return command;
     }
 
-    public Request(String request) {
-        this.request = request;
+    public Object getData(){
+        return data;
     }
 
-    public Request(String request, LinkedList<Task> journal) {
-        this.journal = journal;
+    public Request(){ }
+
+    public Request(String command, Object data){
+        this.command = command;
+        this.data = data;
     }
 
-    public List<Task> getJournal() {
-        return journal;
-    }
-
-    public String getRequest() {
-        return request;
-    }
 }
